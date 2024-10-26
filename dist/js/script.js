bfdfb5a67af192b084d1e90c90931a3f0ac42dcd
Dimasnfl <dimasnaufalxyz@gmail.com>
2024-10-26 20:25:00 +0700

    add dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function (params) {
     const header = document.querySelector('header');
     const navFixed = header.offsetTop;
+    const scrollTop = document.querySelector('#scroll-top');
 
     if (window.scrollY > navFixed) {
         header.classList.add('nav-fixed');
+        scrollTop.classList.remove('hidden');
+        scrollTop.classList.add('flex');
     } else {
         header.classList.remove('nav-fixed');
+        scrollTop.classList.remove('flex');
+        scrollTop.classList.add('hidden');
     }
 };
 
@@ -19,3 +24,32 @@ navMobile.addEventListener('click', function (params) {
     navMenu.classList.toggle('hidden');
 
 });
+
+// close nav mobile active on click outside button
+window.addEventListener('click', function (e) {
+    if (e.target != navMobile && e.target != navMenu) {
+        navMobile.classList.remove('nav-mobile-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+//dark mode toggle
+const darkToggle = document.querySelector('#dark-mode-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function (params) {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+//dark mode toggle position when change theme
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -8,7 +9,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#0ea5e9',
+        primary: '#06b6d4',
         dark: '#0f172a',
         secondary: '#64748b',
       },
@@ -17,6 +18,6 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: ["prettier-plugin-tailwindcss"],
 }
 
